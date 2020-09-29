99bc471438ca2939056d896a361bf1c942a36b75
Andrew Falaleev <andrew.falaleev@gmail.com>
2020-09-29 11:58:57 +0700

    Fix timeout for docker-compose

--- start_light.sh
+++ start_light.sh
@@ -12,7 +12,7 @@ fi
 . "$script_path/env.sh"
 
 if [[ "$1" == "cleanup" ]]; then
-    ( cd $CYBER_DATA; docker-compose -p cyberway -f $CYBER_COMPOSE -t 120 down || exit 1 )
+    ( cd $CYBER_DATA; docker-compose -p cyberway -f $CYBER_COMPOSE down -t 120 || exit 1 )
     docker volume rm cyberway-mongodb-data
     docker volume rm cyberway-nodeos-data
     docker volume rm cyberway-queue
@@ -22,7 +22,7 @@ elif [[ "$1" == "up" ]]; then
     ( cd $CYBER_DATA; docker-compose -p cyberway -f $CYBER_COMPOSE up -t 120 -d || exit 1 )
     exit 0
 elif [[ "$1" == "down" ]]; then
-    ( cd $CYBER_DATA; docker-compose -p cyberway -f $CYBER_COMPOSE -t 120 down || exit 1 )
+    ( cd $CYBER_DATA; docker-compose -p cyberway -f $CYBER_COMPOSE down -t 120 || exit 1 )
     exit 0
 fi
 
