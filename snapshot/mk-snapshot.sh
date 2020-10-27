152cc4df77bf9937083d107c205629962ddebeac
Andrew Falaleev <andrew.falaleev@gmail.com>
2020-10-27 18:22:05 +0700

    Add stopping of nodeosd before snapshort

--- snapshot/mk-snapshot.sh
+++ snapshot/mk-snapshot.sh
@@ -9,6 +9,8 @@ SNAPSHOT_DIR=snapshot-$NOW-$CYBERWAY_VERSION
 
 mkdir -p $SNAPSHOT_DIR
 
+sudo docker stop -t 120 state-reader || true
+sudo docker stop -t 120 nodeosd || true
 sudo ./start_full_node.sh down
 
 sudo docker run --rm -ti -v `readlink -f $SNAPSHOT_DIR`:/host:rw -v cyberway-nodeos-data:/data:ro cyberway/cyberway:$CYBERWAY_VERSION tar -cPvf /host/nodeos.tar /data
