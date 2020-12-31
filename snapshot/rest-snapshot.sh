14dc03997aa15ce3e847dd3a0982fd8a7b30ba09
Andrew Falaleev <andrew.falaleev@gmail.com>
2020-12-31 10:55:00 +0700

    Add rest-snapshot

---
+++ snapshot/rest-snapshot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -xe
+
+SNAPSHOT_DIR=$1
+CYBERWAY_VERSION=v2.1.1
+
+error() {
+   echo $1
+   exit 1
+}
+
+[ "x$SNAPSHOT_DIR" != "x" ] || error "SNAPSHOT_DIR is not defined"
+[ -f "$SNAPSHOT_DIR"/nodeos.tar.bz2 ] || error "no nodeos.tar.bz2"
+[ -f "$SNAPSHOT_DIR"/mongodb.tar.bz2 ] || error "no mongodb.tar.bz2"
+[ -f "$SNAPSHOT_DIR"/nats.tar.bz2 ] || error "no nats.tar.bz2"
+
+docker stop -t 120 state-reader || true
+docker stop -t 120 nodeosd || true
+./start_full_node.sh down
+
+docker volume rm cyberway-nodeos-data || true
+docker volume create cyberway-nodeos-data || true
+
+docker volume rm cyberway-mongodb-data || true
+docker volume create cyberway-mongodb-data || true
+
+docker volume rm cyberway-nats-data || true
+docker volume create cyberway-nats-data || true
+
+sudo docker run --rm -ti -v `readlink -f $SNAPSHOT_DIR`:/host:ro -v cyberway-nodeos-data:/data:rw cyberway/cyberway:$CYBERWAY_VERSION tar -xPvf /host/nodeos.tar.bz2
+sudo docker run --rm -ti -v `readlink -f $SNAPSHOT_DIR`:/host:ro -v cyberway-mongodb-data:/data:rw cyberway/cyberway:$CYBERWAY_VERSION tar -xPvf /host/mongodb.tar.bz2
+sudo docker run --rm -ti -v `readlink -f $SNAPSHOT_DIR`:/host:ro -v cyberway-nats-data:/data:rw cyberway/cyberway:$CYBERWAY_VERSION tar -xPvf /host/nats.tar.bz2
